7645c34bc2dc61bc4bb90e41fcdef9b063ff16ce
Go Sagawa <drunkturtle@gmail.com>
2019-12-09 19:20:56 +0900

    make testcase

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,4 +1,53 @@
 package a
 
-var Gopher int // want "name of identifier must not be ’Gopher’"
-var gopher int // OK
+import (
+	"github.com/jinzhu/gorm"
+)
+
+// expected style
+func f1() (*User, error) {
+
+	db, err := getConnection()
+	if err != nil {
+		return nil, err
+	}
+
+	u := User{}
+	db = db.Where("column_a = xxx")
+	db = db.Where("column_b = xxx")
+	db = db.Find(&u)
+
+	if db.RecordNotFound() {
+		return nil, nil
+	}
+	return &u, db.Error
+}
+
+// using pipe
+func f2() (*User, error) {
+
+	db, err := getConnection()
+	if err != nil {
+		return nil, err
+	}
+
+	u := User{}
+	db = db.Where("column_a = xxx").Where("column_b = xxx")
+	db = db.Find(&u)
+
+	if db.RecordNotFound() {
+		return nil, nil
+	}
+	return &u, db.Error
+}
+
+func getConnection() (*gorm.DB, error) {
+	return nil, nil
+}
+
+// User represents a row from 'test.users'.
+type User struct {
+	ID      int `json:"id"`       // id
+	ColumnA int `json:"column_a"` // column_a
+	ColumnB int `json:"column_b"` // column_b
+}
